6f2b58e45852c23580f78c2f45313e9a53a3bde1
Adhavane Moudougannane <adhavane.moudougannane@etu.utc.fr>
2024-04-08 12:12:16 +0200

    Add new files and configurations for the chat application

---
+++ chatapp/src/test/java/fr/utc/sr03/chatapp/ChatappApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.utc.sr03.chatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
