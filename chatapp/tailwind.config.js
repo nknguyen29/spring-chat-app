6e2742cbef867d6219545f40a660eaee1801b5fe
Adhavane Moudougannane <adhavane.moudougannane@etu.utc.fr>
2024-05-02 00:39:26 +0200

    Update docker-compose.yml and configure MySQL database in application.properties

--- chatapp/tailwind.config.js
+++ chatapp/tailwind.config.js
@@ -1,17 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/main/resources/**/*.{html,js}"],
-  safelist: [
-    'underline',
-    'bg-gray-100'
-  ],
+  safelist: ["underline", "bg-gray-100"],
   theme: {
     extend: {},
     container: {
       center: true,
-    }
+    },
   },
-  plugins: [
-    require('@tailwindcss/forms')
-  ]
-}
+  plugins: [require("@tailwindcss/forms")],
+};
