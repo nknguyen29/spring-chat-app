114b64d6cd1bb6038e8d73198fb1b88d8c9adda6
Adhavane Moudougannane <adhavane.moudougannane@etu.utc.fr>
2024-05-01 15:52:43 +0200

    Add findByUserAndChatroom method to ChatroomUserRepository

--- chatapp/src/main/java/fr/utc/sr03/chatapp/repository/ChatroomUserRepository.java
+++ chatapp/src/main/java/fr/utc/sr03/chatapp/repository/ChatroomUserRepository.java
@@ -1,7 +1,11 @@
 package fr.utc.sr03.chatapp.repository;
 
+import fr.utc.sr03.chatapp.entity.Chatroom;
 import fr.utc.sr03.chatapp.entity.ChatroomUser;
+import fr.utc.sr03.chatapp.entity.User;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface ChatroomUserRepository extends CrudRepository<ChatroomUser, Long> {
+    ChatroomUser findByUserAndChatroom(User user, Chatroom chatroom);
 }
