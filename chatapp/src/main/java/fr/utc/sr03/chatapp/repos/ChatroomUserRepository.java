0df631d9321bb738c59cfb9d6812561c0f8050f2
Adhavane Moudougannane <adhavane.moudougannane@etu.utc.fr>
2024-05-02 01:00:13 +0200

    update 2 files, move 1 file and delete 2 files

--- chatapp/src/main/java/fr/utc/sr03/chatapp/repos/ChatroomUserRepository.java
+++ chatapp/src/main/java/fr/utc/sr03/chatapp/repos/ChatroomUserRepository.java
@@ -1,11 +1,12 @@
-package fr.utc.sr03.chatapp.repository;
+package fr.utc.sr03.chatapp.repos;
 
+import fr.utc.sr03.chatapp.domain.ChatroomUser;
+import fr.utc.sr03.chatapp.domain.Chatroom;
+import fr.utc.sr03.chatapp.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
-import fr.utc.sr03.chatapp.domain.Chatroom;
-import fr.utc.sr03.chatapp.domain.ChatroomUser;
-import fr.utc.sr03.chatapp.domain.User;
-
 public interface ChatroomUserRepository extends CrudRepository<ChatroomUser, Long> {
+
     ChatroomUser findByUserAndChatroom(User user, Chatroom chatroom);
+
 }
--- chatapp/src/main/java/fr/utc/sr03/chatapp/repository/ChatroomRepository.java
+++
@@ -1,8 +0,0 @@
-package fr.utc.sr03.chatapp.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import fr.utc.sr03.chatapp.domain.Chatroom;
-
-public interface ChatroomRepository extends CrudRepository<Chatroom, Long> {
-}
