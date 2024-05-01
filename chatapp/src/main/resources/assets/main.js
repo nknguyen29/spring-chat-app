ad29464574ac5ad0bb2128de6028c213f3d9a4c7
Adhavane Moudougannane <adhavane.moudougannane@etu.utc.fr>
2024-05-01 17:33:49 +0200

    Add main.js, main.scss, and style.css files for chatapp module

---
+++ chatapp/src/main/resources/assets/main.js
@@ -0,0 +1,14 @@
+/**
+ * Hello! 👋
+ * Welcome to your app's main JavaScript file.
+ *
+ * This is the main entry point for the chatapp module.
+ * From here, you can import other JavaScript files, stylesheets, or any other assets.
+ * This file is bundled by Vite and loaded by the chatapp module.
+ */
+
+// Import the styles from the main.scss file
+import "./styles/main.scss";
+
+import imgUrl from './images/vite.svg';
+document.getElementById('vite-img').src = imgUrl;
