cea556e7a09591493882822c3b5738d848c0cadc
Adhavane Moudougannane <adhavane.moudougannane@etu.utc.fr>
2024-05-03 15:30:54 +0200

    Test the getRequestUri method of WebAdvice class

--- chatapp/src/main/java/fr/utc/sr03/chatapp/util/WebAdvice.java
+++ chatapp/src/main/java/fr/utc/sr03/chatapp/util/WebAdvice.java
@@ -10,6 +10,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 @ControllerAdvice
 public class WebAdvice {
 
+    /**
+     * Add the request URI to the model.
+     * 
+     * @param request the HTTP request
+     * @return the request URI
+     * @see https://stackoverflow.com/a/75241636
+     */
     @ModelAttribute("requestUri")
     public String getRequestUri(final HttpServletRequest request) {
         return request.getRequestURI();
