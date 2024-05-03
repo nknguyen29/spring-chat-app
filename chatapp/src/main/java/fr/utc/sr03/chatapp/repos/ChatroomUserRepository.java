546a7f6aa47115879da8007758e67d31059d033b
Adhavane Moudougannane <adhavane.moudougannane@etu.utc.fr>
2024-05-03 19:31:39 +0200

    Replace CrudRepository with JpaRepository

--- chatapp/src/main/java/fr/utc/sr03/chatapp/repos/ChatroomUserRepository.java
+++ chatapp/src/main/java/fr/utc/sr03/chatapp/repos/ChatroomUserRepository.java
@@ -3,9 +3,9 @@ package fr.utc.sr03.chatapp.repos;
 import fr.utc.sr03.chatapp.domain.ChatroomUser;
 import fr.utc.sr03.chatapp.domain.Chatroom;
 import fr.utc.sr03.chatapp.domain.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChatroomUserRepository extends CrudRepository<ChatroomUser, Long> {
+public interface ChatroomUserRepository extends JpaRepository<ChatroomUser, Long> {
 
     ChatroomUser findByUserAndChatroom(User user, Chatroom chatroom);
 
--- chatapp/src/main/java/fr/utc/sr03/chatapp/repos/UserRepository.java
+++ chatapp/src/main/java/fr/utc/sr03/chatapp/repos/UserRepository.java
@@ -1,12 +1,12 @@
 package fr.utc.sr03.chatapp.repos;
 
 import fr.utc.sr03.chatapp.domain.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByEmail(String email);
     boolean existsByEmailIgnoreCase(String email);
