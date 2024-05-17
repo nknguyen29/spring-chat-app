15f8fa91622fb2b2da41c4c524974925aa103275
Adhavane Moudougannane <adhavane.moudougannane@etu.utc.fr>
2024-05-18 01:40:30 +0200

    chore: Add URI components builder to model in WebAdvice

--- chatapp/src/main/java/fr/utc/sr03/chatapp/util/WebAdvice.java
+++ chatapp/src/main/java/fr/utc/sr03/chatapp/util/WebAdvice.java
@@ -4,6 +4,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import org.springframework.web.util.UriComponentsBuilder;
+
 /**
  * Provide attributes available in all templates.
  */
@@ -31,6 +33,34 @@ public class WebAdvice {
     @ModelAttribute("requestQueryString")
     public String getRequestQueryString(final HttpServletRequest request) {
         return request.getQueryString();
+    }
+
+    @ModelAttribute("requestUriWithQueryString")
+    public String getRequestUriWithQueryString(final HttpServletRequest request) {
+        return request.getRequestURI() + (request.getQueryString() != null ? "?" + request.getQueryString() : ""); 
+    }
+
+    @ModelAttribute("requestUrl")
+    public String getRequestUrl(final HttpServletRequest request) {
+        return request.getRequestURL().toString();
+    }
+
+    @ModelAttribute("requestUrlWithQueryString")
+    public String getRequestUrlWithQueryString(final HttpServletRequest request) {
+        return request.getRequestURL().toString() + (request.getQueryString() != null ? "?" + request.getQueryString() : ""); 
+    }
+
+    /**
+     * Add the URI components builder to the model.
+     * 
+     * @param request the HTTP request
+     * @return the URI components builder
+     * @see https://stackoverflow.com/q/45594238
+     */
+    @ModelAttribute("uriComponentsBuilder")
+    public UriComponentsBuilder getUriComponentsBuilder(final HttpServletRequest request) {
+        final String requestUrlWithQueryString = request.getRequestURL().toString() + (request.getQueryString() != null ? "?" + request.getQueryString() : "");
+        return UriComponentsBuilder.fromUriString(requestUrlWithQueryString);
     }
 
     @ModelAttribute("isDevserver")
