5eb3c671927c70459ddcdc0ddd01494d40f7a123
Adhavane Moudougannane <adhavane.moudougannane@etu.utc.fr>
2024-05-21 03:17:16 +0200

    refactor: Update configuration classes and organize imports

--- chatapp/src/main/java/fr/utc/sr03/chatapp/config/DomainConfig.java
+++ chatapp/src/main/java/fr/utc/sr03/chatapp/config/DomainConfig.java
@@ -5,6 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+
+/**
+ * This class is used to configure the domain package
+ * It is used to scan the domain package for entities and repositories
+ * It is used to enable the transaction management
+ */
 @Configuration
 @EntityScan("fr.utc.sr03.chatapp.domain")
 @EnableJpaRepositories("fr.utc.sr03.chatapp.repos")
