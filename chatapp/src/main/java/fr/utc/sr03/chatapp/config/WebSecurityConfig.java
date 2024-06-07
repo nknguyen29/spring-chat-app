5910d99c429e1ce3db46befdaae7e6c3187b7e54
Adhavane Moudougannane <adhavane.moudougannane@etu.utc.fr>
2024-06-08 01:58:37 +0200

    feat: Add JWT authentication and authorization support

--- chatapp/src/main/java/fr/utc/sr03/chatapp/config/WebSecurityConfig.java
+++ chatapp/src/main/java/fr/utc/sr03/chatapp/config/WebSecurityConfig.java
@@ -5,17 +5,21 @@ import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import fr.utc.sr03.chatapp.util.CustomLoginFailureHandler;
 import fr.utc.sr03.chatapp.util.CustomLoginSuccessHandler;
+import fr.utc.sr03.chatapp.util.JwtRequestFilter;
 
 
 @Configuration
@@ -34,7 +38,21 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain configure(final HttpSecurity http) throws Exception {
+    @Order(value = 1)
+    public SecurityFilterChain apiFilterChain(final HttpSecurity http) throws Exception {
+        return http.cors(withDefaults())
+                .csrf(withDefaults())
+                .securityMatcher("/api/**")
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/**").permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(new JwtRequestFilter(), UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public SecurityFilterChain formLoginFilterChain(final HttpSecurity http) throws Exception {
         return http.cors(withDefaults())
                 .csrf(withDefaults())
                 .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
---
+++ chatapp/src/main/java/fr/utc/sr03/chatapp/util/JwtRequestFilter.java
@@ -0,0 +1,62 @@
+package fr.utc.sr03.chatapp.util;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import fr.utc.sr03.chatapp.model.JwtUserDetails;
+import fr.utc.sr03.chatapp.service.JwtTokenService;
+import fr.utc.sr03.chatapp.service.JwtUserDetailsService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtTokenService jwtTokenService;
+
+    @Autowired
+    private JwtUserDetailsService jwtUserDetailsService;
+
+    @Override
+    protected void doFilterInternal(@NonNull final HttpServletRequest request,
+            @NonNull final HttpServletResponse response,
+            @NonNull final FilterChain chain) throws IOException, ServletException {
+        // look for Bearer auth header
+        final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (header == null || !header.startsWith("Bearer ")) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        final String token = header.substring(7);
+        final String username = jwtTokenService.validateTokenAndGetEmail(token);
+        if (username == null) {
+            // validation failed or token expired
+            chain.doFilter(request, response);
+            return;
+        }
+
+        // set user details on spring security context
+        final JwtUserDetails userDetails = jwtUserDetailsService.loadUserByUsername(username);
+        final UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                userDetails, null, userDetails.getAuthorities());
+        authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        // continue with authenticated user
+        chain.doFilter(request, response);
+    }
+
+}
