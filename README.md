94efbad49fd75083f1835126b6c5bd97863d54e1
Adhavane Moudougannane <adhavane.moudougannane@etu.utc.fr>
2024-06-07 20:27:27 +0200

    chore: Update WebSocketConfig to allow multiple origins for web socket connection

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # SR03 - Spring Chat App
 
+
+
 ## Getting started
 
 To make it easy for you to get started with GitLab, here's a list of recommended next steps.
--- chatapp/src/main/java/fr/utc/sr03/chatapp/config/WebSocketConfig.java
+++ chatapp/src/main/java/fr/utc/sr03/chatapp/config/WebSocketConfig.java
@@ -7,21 +7,24 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-  @Override
-  public void configureMessageBroker(@NonNull MessageBrokerRegistry config) {
-    config.enableSimpleBroker("/topic");
-    config.setApplicationDestinationPrefixes("/app");
-  }
+    // allowed origins for the web socket connection
+    private static final String[] allowedOrigins = { "http://localhost:3000" };
 
-  @Override
-  public void registerStompEndpoints(@NonNull StompEndpointRegistry registry) {
-    registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000").withSockJS();
-    // pay attention to the allowed origins localhost:3000
-  }
+    @Override
+    public void configureMessageBroker(@NonNull MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(@NonNull StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOrigins(allowedOrigins).withSockJS();
+        // pay attention to the allowed origins localhost:3000
+    }
 
 }
-
