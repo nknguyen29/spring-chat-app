c679c99018977b904f77ae775eb21640ba8b348e
Adhavane Moudougannane <adhavane.moudougannane@etu.utc.fr>
2024-06-08 19:52:33 +0200

    update ChatroomResource.java and UserResource.java

--- chatapp/src/main/java/fr/utc/sr03/chatapp/rest/ChatroomResource.java
+++ chatapp/src/main/java/fr/utc/sr03/chatapp/rest/ChatroomResource.java
@@ -1,23 +1,19 @@
 package fr.utc.sr03.chatapp.rest;
 
-import fr.utc.sr03.chatapp.model.ChatroomDTO;
-import fr.utc.sr03.chatapp.model.ChatroomPublicDTO;
-import fr.utc.sr03.chatapp.model.ChatroomWithoutUserDTO;
-import fr.utc.sr03.chatapp.service.ChatroomService;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import jakarta.validation.Valid;
 import java.util.List;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import fr.utc.sr03.chatapp.model.ChatroomDTO;
+import fr.utc.sr03.chatapp.model.ChatroomPublicDTO;
+import fr.utc.sr03.chatapp.service.ChatroomService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 
 @RestController
