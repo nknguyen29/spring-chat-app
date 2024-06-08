c25cb36ff9f8cf45521a9a986f1704e967e461b5
Nam Khanh Nguyen <namkhanhnguyen296@gmail.com>
2024-06-08 13:06:44 +0200

    chore: Refactor Logout component to use useEffect for token removal and header deletion

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,31 +1,22 @@
-import React, { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { AuthContext } from './AuthContext';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
 
 const Logout = () => {
-  const navigate = useNavigate();
-
   const { setIsAuthenticated } = useContext(AuthContext);
-  
-  // Clear the token from storage
-  sessionStorage.removeItem('token');
 
-  // Remove the Authorization header
-  delete axios.defaults.headers.common['Authorization'];
+  useEffect(() => {
+    // Clear the token from storage
+    sessionStorage.removeItem('token');
 
-  // Update the authentication state
-  setIsAuthenticated(false);
+    // Remove the Authorization header
+    delete axios.defaults.headers.common['Authorization'];
 
-  // Redirect to the login page
-  navigate('/');
+    // Update the authentication state
+    setIsAuthenticated(false);
+  }, [setIsAuthenticated]); 
 
-
-  return (
-    <div>
-        <h1>Logging out, please wait</h1>
-    </div>
-  );
+  return null; 
 };
 
 export default Logout;
