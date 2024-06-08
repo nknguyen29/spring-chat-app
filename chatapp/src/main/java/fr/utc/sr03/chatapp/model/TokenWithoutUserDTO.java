aa6182d346120eb72d5fd65679475c89097a2e73
Adhavane Moudougannane <adhavane.moudougannane@etu.utc.fr>
2024-06-08 18:15:11 +0200

    feat: Add TokenMapper and TokenWithoutUserDTO classes

--- chatapp/src/main/java/fr/utc/sr03/chatapp/model/TokenWithoutUserDTO.java
+++ chatapp/src/main/java/fr/utc/sr03/chatapp/model/TokenWithoutUserDTO.java
@@ -1,5 +1,86 @@
 package fr.utc.sr03.chatapp.model;
 
+import java.sql.Timestamp;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+
 public class TokenWithoutUserDTO {
-    
+
+    private Long id;
+
+    @NotNull
+    @Size(min = 512)
+    private String token;
+
+    @NotNull
+    @Pattern(regexp = "^(access|refresh|reset|confirm)$")
+    private String type;
+
+    @NotNull
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssXXX")
+    private Timestamp createdAt;
+
+    @NotNull
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssXXX")
+    private Timestamp expiresAt;
+
+    public TokenWithoutUserDTO() {
+    }
+
+    public TokenWithoutUserDTO(
+            String token,
+            String type,
+            Timestamp createdAt,
+            Timestamp expiresAt) {
+        this.token = token;
+        this.type = type;
+        this.createdAt = createdAt;
+        this.expiresAt = expiresAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(Timestamp expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+
 }
