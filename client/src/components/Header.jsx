47bd6788301c664ffdb04c65483226e9261dc647
Nam Khanh Nguyen <namkhanhnguyen296@gmail.com>
2024-06-17 01:15:49 +0200

    feat: Added a refresh data now button to fetch new data

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import {
-  CircleUser,
-} from "lucide-react"
+import { CircleUser, RefreshCw } from "lucide-react";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 import {
   DropdownMenu,
@@ -14,16 +12,59 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from "@/components/ui/dropdown-menu";
 
-import ThemeModeToggle from './ThemeModeToggle';
+import ThemeModeToggle from "./ThemeModeToggle";
 
-export default function App({user}) {
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+export default function App({ user }) {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: () => {},
+    onError: (error) => {
+      console.log("[Header] onError: ", error);
+    },
+    onSettled: (data, error) => {
+      console.log("[Header] onSettled: ", data, error);
+      // invalidate the query to refetch the data
+      queryClient.invalidateQueries([
+        { queryKey: ["chatrooms"] },
+        { queryKey: ["userChatrooms"] },
+        { queryKey: ["users"] },
+      ]);
+    },
+  });
+
+  function handleRefresh() {
+    mutation.mutate();
+  }
+
   return (
     <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
-      <div className="w-full flex-1">
-      </div>
-      <ThemeModeToggle/>
+      <div className="w-full flex-1"></div>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" size="icon">
+            <RefreshCw className="h-5 w-5" />
+            <span className="sr-only">Refresh Data</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>
+            <div>Refresh Data</div>
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <Button variant="ghost" onClick={handleRefresh}>
+              Refresh Now
+            </Button>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <ThemeModeToggle />
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="outline" size="icon">
@@ -33,16 +74,16 @@ export default function App({user}) {
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
           <DropdownMenuLabel>
-            <div>{user.firstName} {user.lastName}</div>
+            <div>
+              {user.firstName} {user.lastName}
+            </div>
           </DropdownMenuLabel>
           <DropdownMenuSeparator />
           <Link to="/logout">
-            <DropdownMenuItem>
-                Logout
-            </DropdownMenuItem>
+            <DropdownMenuItem>Logout</DropdownMenuItem>
           </Link>
         </DropdownMenuContent>
       </DropdownMenu>
     </header>
-  )
+  );
 }
